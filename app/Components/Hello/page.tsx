6edafd6e596257ac00206ac4ccc780489b5144bf
b1sh4l <sudarshansahabishal@gmail.com>
2024-04-09 05:38:30 +0600

    Inspecting

--- app/Components/Hello/page.tsx
+++ app/Components/Hello/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import eventorbdLogo from '../../assets/eventorbdLogo.svg';
+import Image from 'next/image';
 
 
 
@@ -6,6 +8,7 @@ const Hello = () => {
   return (
     <div>
       
+      <Image src={eventorbdLogo} alt="EventorBD Logo" width={200} height={200} />
       EventorBD!!!
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Hello from './Components/Hello/page';
-import Image from 'next/image';
-import eventorbdLogo from '../public/eventorbdLogo.svg';
+
 
 export default function Home() {
   return (
     <main>
-      <Image src={eventorbdLogo} alt="EventorBD Logo" />
       <Hello />
     </main>
   );
